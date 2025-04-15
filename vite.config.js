fe64e563dec588aea08d358d3b9ea3a8e5033143
HectorFylkner <diurnal.fawn_3b@icloud.com>
2025-04-16 00:04:07 +0200

    fix: Configure correct base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add the base path for GitHub Pages deployment
+  base: '/nomapod/', 
   plugins: [react()],
 }) 
