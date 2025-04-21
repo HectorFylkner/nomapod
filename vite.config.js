61d4797867f41384f0fa48ad1ee32f6e1deef412
HectorFylkner <diurnal.fawn_3b@icloud.com>
2025-04-21 13:43:57 +0200

    fix: remove base path from vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add the base path for GitHub Pages deployment
-  base: '/nomapod/', 
   plugins: [react()],
 }) 
